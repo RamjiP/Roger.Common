413208f81a44bee18a06a989e1ed372e678edf03
Ramji Piramanayagam <Ramji.Piramanayagam@vivaenergy.com.au>
2019-02-13 10:58:37 +1100

    introduced common project

---
+++ Roger.Common/Models/ITokenPagedResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Roger.Common.Models
+{
+    public interface ITokenPagedResult<T>
+    {
+        List<T> Data { get; set; }
+
+        string Token { get; set; }
+    }
+}
---
+++ Roger.Common/Models/TokenPagedResult.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Roger.Common.Models
+{
+    public class TokenPagedResult<T> : ITokenPagedResult<T>
+    {
+        public List<T> Data { get; set; }
+        public string Token { get; set; }
+
+        public TokenPagedResult()
+        {
+            Data = new List<T>();
+        }
+    }
+}
