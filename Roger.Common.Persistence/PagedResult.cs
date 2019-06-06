3efdc1374bc1297d75c6b6b9aab16d5152e51689
Ramji Piramanayagam <ramji.piramanayagam@vivaenergy.com.au>
2019-06-07 08:49:57 +1000

    Introduced paged result

---
+++ Roger.Common.Persistence/PagedResult.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Roger.Common.Persistence
+{
+    public class PagedResult<T> : IPagedResult<T>
+    {
+        public List<T> Data { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public bool HasNextPage { get; set; }
+
+        public PagedResult()
+        {
+            Data = new List<T>();
+        }
+    }
+}
