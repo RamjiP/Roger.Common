5ac43310e8e5921e3f36d880437886c6d2c0494d
Ramji Piramanayagam <Ramji.Piramanayagam@vivaenergy.com.au>
2019-06-12 18:48:51 +1000

    Changes to Topic service interface

--- Roger.Common.Messaging/ITopicService.cs
+++ Roger.Common.Messaging/ITopicService.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Roger.Common.Messaging
 {
     public interface ITopicService<T>
+        where T: new()
     {
-        Task SendAsync(T obj);
+        Task SendAsync(T obj, IDictionary<string, object> properties = null);
     }
 }
