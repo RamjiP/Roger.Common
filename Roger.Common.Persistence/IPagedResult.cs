29498e12cf0cf8fe5a62ad6fc090941bb94e9807
Ramji Piramanayagam <Ramji.Piramanayagam@vivaenergy.com.au>
2019-06-13 11:29:24 +1000

    Introduced total paged count

--- Roger.Common.Persistence/IPagedResult.cs
+++ Roger.Common.Persistence/IPagedResult.cs
@@ -4,6 +4,7 @@ namespace Roger.Common.Persistence
 {
     public interface IPagedResult<T>
     {
+        int TotalRowCount { get; set; }
         List<T> Data { get; set; }
         int PageNumber { get; set; }
         int PageSize { get; set; }
--- Roger.Common.Persistence/PagedResult.cs
+++ Roger.Common.Persistence/PagedResult.cs
@@ -4,6 +4,7 @@ namespace Roger.Common.Persistence
 {
     public class PagedResult<T> : IPagedResult<T>
     {
+        public int TotalRowCount { get; set; }
         public List<T> Data { get; set; }
         public int PageNumber { get; set; }
         public int PageSize { get; set; }
